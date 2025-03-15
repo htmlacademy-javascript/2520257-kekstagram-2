2addb475b7e497f69f7785df390837926f9ccf56
Ivan Shapkov <stfx@live.ru>
2025-03-16 00:31:13 +0300

    правки

--- js/modal-form/scale-control.js
+++ js/modal-form/scale-control.js
@@ -1,47 +1,41 @@
 // Элементы управления масштабом
 
-const uploadOverlay = document.querySelector('.img-upload__overlay');
-
-const scaleControl = uploadOverlay.querySelectorAll('.scale__control');
-const uploadImage = uploadOverlay.querySelector('.img-upload__preview img');
+const scaleValue = document.querySelector('.scale__control--value');
+const decreaseButton = document.querySelector('.scale__control--smaller');
+const increaseButton = document.querySelector('.scale__control--bigger');
+const uploadImage = document.querySelector('.img-upload__preview img');
 
 // Константы для масштабирования
 
 const MAX_SCALE = 100;
 const MIN_SCALE = 25;
 const SCALE_STEP = 25;
-let currentValue = 100;
+let currentValue = MAX_SCALE;
 
 // Обновление значения масштаба
 
 const changeScale = () => {
-  scaleControl[2].removeAttribute('disabled');
-  scaleControl[0].removeAttribute('disabled');
-  scaleControl[1].value = `${currentValue}%`;
+  scaleValue.value = `${currentValue}%`;
   uploadImage.style.transform = `scale(${currentValue / 100})`;
 
-  if (currentValue >= MAX_SCALE) {
-    scaleControl[2].setAttribute('disabled', '');
-  }
-  if (currentValue <= MIN_SCALE) {
-    scaleControl[0].setAttribute('disabled', '');
-  }
+  increaseButton.disabled = currentValue >= MAX_SCALE;
+  decreaseButton.disabled = currentValue <= MIN_SCALE;
 };
 
 // Сброс масштаба
 
 const resetScale = () => {
   currentValue = MAX_SCALE;
-  scaleControl[1].value = `${currentValue}%`;
+  scaleValue.value = `${currentValue}%`;
   uploadImage.style.transform = 'scale(1.00)';
-  scaleControl[2].removeAttribute('disabled');
-  scaleControl[0].removeAttribute('disabled');
-  scaleControl[2].setAttribute('disabled', '');
+  increaseButton.disabled = false;
+  decreaseButton.disabled = false;
+  changeScale();
 };
 
 // Кнопка Увеличить
 
-scaleControl[2].addEventListener('click', () => {
+increaseButton.addEventListener('click', () => {
   if (currentValue < MAX_SCALE) {
     currentValue += SCALE_STEP;
     changeScale();
@@ -50,7 +44,7 @@ scaleControl[2].addEventListener('click', () => {
 
 // Кнопка Уменьшить
 
-scaleControl[0].addEventListener('click', () => {
+decreaseButton.addEventListener('click', () => {
   if (currentValue > MIN_SCALE) {
     currentValue -= SCALE_STEP;
     changeScale();
