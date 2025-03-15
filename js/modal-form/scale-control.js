b337d22ed0984424bf41bd4770d0f0a6ace98f14
Ivan Shapkov <stfx@live.ru>
2025-03-15 23:14:36 +0300

    исправляет ошибку

--- js/modal-form/scale-control.js
+++ js/modal-form/scale-control.js
@@ -1,53 +1,60 @@
-const uploadOverlay = document.querySelector('.img-upload__overlay');
+// Элементы управления масштабом
 
-//изменение масштаба изображения
+const uploadOverlay = document.querySelector('.img-upload__overlay');
 
 const scaleControl = uploadOverlay.querySelectorAll('.scale__control');
 const uploadImage = uploadOverlay.querySelector('.img-upload__preview img');
+
+// Константы для масштабирования
 
 const MAX_SCALE = 100;
 const MIN_SCALE = 25;
 const SCALE_STEP = 25;
 let currentValue = 100;
 
-// Обновление значения
+// Обновление значения масштаба
 
 const changeScale = () => {
-  scaleControl[2].removeAttribute('disabled', '');
-  scaleControl[0].removeAttribute('disabled', '');
+  scaleControl[2].removeAttribute('disabled');
+  scaleControl[0].removeAttribute('disabled');
   scaleControl[1].value = `${currentValue}%`;
   uploadImage.style.transform = `scale(${currentValue / 100})`;
+
+  if (currentValue >= MAX_SCALE) {
+    scaleControl[2].setAttribute('disabled', '');
+  }
+  if (currentValue <= MIN_SCALE) {
+    scaleControl[0].setAttribute('disabled', '');
+  }
 };
 
-// Сброс значения
+// Сброс масштаба
 
 const resetScale = () => {
-  scaleControl.value = '100%';
+  currentValue = MAX_SCALE;
+  scaleControl[1].value = `${currentValue}%`;
   uploadImage.style.transform = 'scale(1.00)';
+  scaleControl[2].removeAttribute('disabled');
+  scaleControl[0].removeAttribute('disabled');
+  scaleControl[2].setAttribute('disabled', '');
 };
 
-// Кнопка увеличить
+// Кнопка Увеличить
 
 scaleControl[2].addEventListener('click', () => {
   if (currentValue < MAX_SCALE) {
     currentValue += SCALE_STEP;
     changeScale();
-  } else {
-    scaleControl[2].setAttribute('disabled', '');
   }
-  return currentValue;
 });
 
-// Кнопка уменьшить
+// Кнопка Уменьшить
 
 scaleControl[0].addEventListener('click', () => {
   if (currentValue > MIN_SCALE) {
     currentValue -= SCALE_STEP;
     changeScale();
-  } else {
-    scaleControl[0].setAttribute('disabled', '');
   }
-  return currentValue;
 });
 
 export { resetScale };
